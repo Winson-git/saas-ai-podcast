201b2148a1b72a4a1af3cb48a80e8e423ea7a83c
Winson Hong <winsonhong2001@gmail.com>
2024-09-05 16:51:12 +0800

    Profile Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,12 +4,15 @@ import PodcastCard from '@/components/PodcastCard'
 import React from 'react'
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { cn } from '@/lib/utils';
+import { useAudio } from '@/providers/AudioProvider';
 
 const Home = () => {
   const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
+  const { audio } = useAudio();
 
   return (
-    <div className='mt-9 flex flex-col gap-9'>
+    <div className={cn('mt-9 flex flex-col gap-9',{'h-[calc(100vh-140px)]': audio?.audioUrl })}>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Treding Podcasts</h1>
         <div className='podcast_grid'>
